ce076527ec53c2ef00b1e086174fb00827be93bb
amerkhali31 <amerkhali31@gmail.com>
2024-12-11 16:46:04 -0600

    Initial Commit

---
+++ MAGR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAGR
+//
+//  Created by Amer Khalil on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
